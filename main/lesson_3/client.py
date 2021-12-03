bb238981bb266ea175cb9a546127d259b00d4f40
Oleksandr Shtyrov <shtyrov89@gmail.com>
2021-12-03 12:20:16 +0200

    lesson_5 done.

--- main/lesson_3/client.py
+++ main/lesson_3/client.py
@@ -4,9 +4,16 @@ import sys
 import json
 import socket
 import time
+import argparse
+import logging
+import logs.client_log_config
+from errors import ReqFieldMissingError
 from common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, \
     RESPONSE, ERROR, DEFAULT_IP_ADDRESS, DEFAULT_PORT
 from common.utils import get_message, send_message
+
+# Инициализация клиентского логера
+CLIENT_LOGGER = logging.getLogger('client')
 
 
 def create_message(account_name='Guest'):
@@ -22,7 +29,19 @@ def create_message(account_name='Guest'):
             ACCOUNT_NAME: account_name
         }
     }
+    CLIENT_LOGGER.debug(f'Сформировано {PRESENCE} сообщение для пользователя {account_name}')
     return out
+
+
+def create_arg_parser():
+    """
+    Создаём парсер аргументов коммандной строки
+    :return:
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('addr', default=DEFAULT_IP_ADDRESS, nargs='?')
+    parser.add_argument('port', default=DEFAULT_PORT, type=int, nargs='?')
+    return parser
 
 
 def server_response_handler(message):
@@ -31,38 +50,51 @@ def server_response_handler(message):
     :param message:
     :return:
     """
+    CLIENT_LOGGER.debug(f'Разбор сообщения от сервера: {message}')
     if RESPONSE in message:
         if message[RESPONSE] == 200:
             return '200 : OK'
         return f'400 : {message[ERROR]}'
-    raise ValueError
+    raise ReqFieldMissingError(RESPONSE)
 
 
 def main():
-    """Загружаем параметы коммандной строки"""
-    try:
-        server_address = sys.argv[1]
-        server_port = int(sys.argv[2])
-        if server_port < 1024 or server_port > 65535:
-            raise ValueError
-    except IndexError:
-        server_address = DEFAULT_IP_ADDRESS
-        server_port = DEFAULT_PORT
-    except ValueError:
-        print('В качестве порта может быть указано только число в диапазоне от 1024 до 65535.')
+    """
+    Загружаем параметы коммандной строки
+    :return:
+    """
+    parser = create_arg_parser()
+    namespace = parser.parse_args(sys.argv[1:])
+    server_address = namespace.addr
+    server_port = namespace.port
+
+    # проверим подходящий номер порта
+    if not 1023 < server_port < 65536:
+        CLIENT_LOGGER.critical(
+            f'Попытка запуска клиента с неподходящим номером порта: {server_port}.'
+            f' Допустимы адреса с 1024 до 65535. Клиент завершается.')
         sys.exit(1)
 
+    CLIENT_LOGGER.info(f'Запущен клиент с парамертами: '
+                       f'адрес сервера: {server_address} , порт: {server_port}')
     # Инициализация сокета и обмен
 
-    transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    transport.connect((server_address, server_port))
-    message_to_server = create_message()
-    send_message(transport, message_to_server)
     try:
+        transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        transport.connect((server_address, server_port))
+        message_to_server = create_message()
+        send_message(transport, message_to_server)
         answer = server_response_handler(get_message(transport))
+        CLIENT_LOGGER.info(f'Принят ответ от сервера {answer}')
         print(answer)
-    except (ValueError, json.JSONDecodeError):
-        print('Не удалось декодировать сообщение сервера.')
+    except json.JSONDecodeError:
+        CLIENT_LOGGER.error('Не удалось декодировать полученную Json строку.')
+    except ConnectionRefusedError:
+        CLIENT_LOGGER.critical(f'Не удалось подключиться к серверу {server_address}:{server_port}, '
+                               f'конечный компьютер отверг запрос на подключение.')
+    except ReqFieldMissingError as missing_error:
+        CLIENT_LOGGER.error(f'В ответе сервера отсутствует необходимое поле '
+                            f'{missing_error.missing_field}')
 
 
 if __name__ == '__main__':
